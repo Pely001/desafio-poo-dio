c040dfc0021986227b1a869dfb3b181145c973a1
Pely001 <pelypedrohenrique1@gmail.com>
2024-05-05 01:45:29 -0300

    Desafio Finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -21,8 +23,31 @@ public class Main {
         mentoria.setDescricao("Mentoria DIO Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp Dio Becas Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+        bootcamp.getConteudos().add(curso2);
+
+        Dev devPedro = new Dev();
+        devPedro.setNome("Pedro");
+        devPedro.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devPedro.getConteudosInscritos());
+        devPedro.progredir();
+        System.out.println("Conteúdos Concluídos" + devPedro.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos" + devPedro.getConteudosInscritos());
+        
+        Dev devTati = new Dev();
+        devTati.setNome("Tati");
+        devTati.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devTati.getConteudosInscritos());
+        devTati.progredir();
+        System.out.println("Conteúdos Concluídos" + devTati.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos" + devTati.getConteudosInscritos());
      }
 }
